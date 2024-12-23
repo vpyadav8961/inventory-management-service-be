26528588211dc00a18761808539710f41e681389
Ved Prakash Yadav <vpyadav8961@gmail.com>
2024-12-23 23:05:01 +0530

    created dummy test end point

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require("express");
-const {createServer} = require("http"); 
+const {createServer} = require("http");
+const {config} = require('dotenv');
+const {Server} = require("socket.io");
+const cors = require('cors');
+config();
 const app = express();
 const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+      origin: process.env.FRONTEND_URL || 'http://localhost:5175',
+      methods: ['GET', 'POST']
+    }
+  });
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/test", (req, res, next) => {
+    console.log("This is the test url.");
+    res.status(200).json({status:"success", message: "Test running successfully"});
+})
+
 
 const PORT = process.env.PORT || 5175;
 httpServer.listen(PORT, () => {
