276b650ea4feb96cb6712245057cf62738fdcca1
Ved Prakash Yadav <vpyadav8961@gmail.com>
2025-01-18 00:18:08 +0530

    Code refactor and done some changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,30 +1,30 @@
 const { body, validationResult } = require('express-validator');
 const User = require('../models/User');
 
-const signupValidationRules = () => {
-  return [
-    body('username')
-      .notEmpty().withMessage('Username is required')
-      .custom(async (username) => {
-        const existingUser = await User.findOne({ where: { username } });
-        if (existingUser) {
-          throw new Error('Username already in use');
-        }
-      }),
-    body('email')
-      .isEmail().withMessage('Enter a valid email address')
-      .custom(async (email) => {
-        const existingUser = await User.findOne({ where: { email } });
-        if (existingUser) {
-          throw new Error('Email already in use');
-        }
-      }),
-    body('password')
-      .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
-    body('role')
-      .isIn(['staff', 'admin']).withMessage('Invalid role')
-  ];
-};
+const signupValidationRules = () => [
+  body('username')
+    .notEmpty().withMessage('Username is required')
+    .bail()
+    .custom(async (username) => {
+      const existingUser = await User.findOne({ where: { username } });
+      if (existingUser) {
+        throw new Error('Username already in use');
+      }
+    }),
+  body('email')
+    .isEmail().withMessage('Enter a valid email address')
+    .bail()
+    .custom(async (email) => {
+      const existingUser = await User.findOne({ where: { email } });
+      if (existingUser) {
+        throw new Error('Email already in use');
+      }
+    }),
+  body('password')
+    .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+  body('role')
+    .isIn(['staff', 'admin']).withMessage('Invalid role')
+];
 
 const validate = (req, res, next) => {
   const errors = validationResult(req);
